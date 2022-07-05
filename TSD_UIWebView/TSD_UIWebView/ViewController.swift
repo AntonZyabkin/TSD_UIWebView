befd393853db60671b58aa31906352c2c7380948
AntonZyabkin <zyabkinantonv@gmail.com>
2022-07-05 14:07:08 +0300

    2 commit

--- TSD_UIWebView/TSD_UIWebView/ViewController.swift
+++ TSD_UIWebView/TSD_UIWebView/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let button : UIButton = {
+        let button = UIButton ()
+        button.backgroundColor = .link
+        button.setTitle("Press to go to our web site", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(didTaped), for: .touchUpInside)
+        button.frame = CGRect (x: 0, y: 0, width: 250, height: 50)
+        button.center = view.center
+        
     }
 
 
+    
+    @objc func didTaped () {
+        
+    }
 }
 
